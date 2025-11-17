b29a0fd527f508cb022a587ce772afb384ffab27
Princeray-18 <rayp11348@gmail.com>
2025-11-17 23:29:38 +0530

    Final

--- Billing_sw_frontend/src/BillManagementApp.jsx
+++ Billing_sw_frontend/src/BillManagementApp.jsx
@@ -28,7 +28,7 @@ import {
 } from "lucide-react";
 import Swal from "sweetalert2";
 const BillManagementApp = () => {
-  const API_BASE_URL = "https://billing-software-8de3.onrender.com";
+  const API_BASE_URL = "https://billing-sw-backendd-main.onrender.com";
 
   const getBillTypeLabel = (type) => {
     const labels = {
